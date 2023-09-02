f67d078ebb289490d71c2f91e781bad58753cd6a
Devessier <baptiste@devessier.fr>
2023-09-02 23:25:26 +0200

    feat: add view transition

--- src/components/AppNavbar.tsx
+++ src/components/AppNavbar.tsx
@@ -284,7 +284,7 @@ export function AppNavbar() {
                 <div className="space-y-6 border-t border-gray-200 px-4 py-6">
                   <div className="flow-root">
                     <a
-                      href="#"
+                      href="/sign-in"
                       className="-m-2 block p-2 font-medium text-gray-900"
                     >
                       Sign in
@@ -292,26 +292,12 @@ export function AppNavbar() {
                   </div>
                   <div className="flow-root">
                     <a
-                      href="#"
+                      href="/sign-up"
                       className="-m-2 block p-2 font-medium text-gray-900"
                     >
                       Create account
                     </a>
                   </div>
-                </div>
-
-                <div className="border-t border-gray-200 px-4 py-6">
-                  <a href="#" className="-m-2 flex items-center p-2">
-                    <img
-                      src="https://tailwindui.com/img/flags/flag-canada.svg"
-                      alt=""
-                      className="block h-auto w-5 flex-shrink-0"
-                    />
-                    <span className="ml-3 block text-base font-medium text-gray-900">
-                      CAD
-                    </span>
-                    <span className="sr-only">, change currency</span>
-                  </a>
                 </div>
               </Dialog.Panel>
             </Transition.Child>
@@ -339,7 +325,7 @@ export function AppNavbar() {
 
               {/* Logo */}
               <div className="ml-4 flex lg:ml-0">
-                <a href="#">
+                <a href="/">
                   <span className="sr-only">Your Company</span>
                   <img
                     className="h-8 w-auto"
@@ -488,37 +474,22 @@ export function AppNavbar() {
               <div className="ml-auto flex items-center">
                 <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
                   <a
-                    href="#"
+                    href="/sign-in"
                     className="text-sm font-medium text-gray-700 hover:text-gray-800"
                   >
                     Sign in
                   </a>
                   <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
                   <a
-                    href="#"
+                    href="/sign-up"
                     className="text-sm font-medium text-gray-700 hover:text-gray-800"
                   >
                     Create account
                   </a>
                 </div>
 
-                <div className="hidden lg:ml-8 lg:flex">
-                  <a
-                    href="#"
-                    className="flex items-center text-gray-700 hover:text-gray-800"
-                  >
-                    <img
-                      src="https://tailwindui.com/img/flags/flag-canada.svg"
-                      alt=""
-                      className="block h-auto w-5 flex-shrink-0"
-                    />
-                    <span className="ml-3 block text-sm font-medium">CAD</span>
-                    <span className="sr-only">, change currency</span>
-                  </a>
-                </div>
-
                 {/* Search */}
-                <div className="flex lg:ml-6">
+                <div className="flex lg:ml-8">
                   <a href="#" className="p-2 text-gray-400 hover:text-gray-500">
                     <span className="sr-only">Search</span>
                     <MagnifyingGlassIcon
