b301e0c4dcfea2665d468a3d685962dce91b2392
Devessier <baptiste@devessier.fr>
2023-09-10 17:22:39 +0200

    fix: allow tailwind to find classes in the json file containing products

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,json}'],
 	theme: {
 		extend: {
 			fontFamily: {
